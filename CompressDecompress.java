29a28fb544b88f5f69b2e434fef7cadacfd3ace8
luvashoo7 <61554374+luvashoo7@users.noreply.github.com>
2023-01-10 11:58:50 +0530

    main

---
+++ CompressDecompress.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.compress.decompress;
+
+/**
+ *
+ * @author PRAGATI PUSHKAR
+ */
+public class CompressDecompress {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ decompress.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.compress.decompress;
+
+/**
+ *
+ * @author PRAGATI PUSHKAR
+ */
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+
+/**
+ *
+ * @author shivenduac
+ */
+public class decompress {
+    public static void method(File file) throws IOException{
+        String fileDirectory= file.getParent();
+        FileInputStream fis= new FileInputStream(file);
+        GZIPInputStream gzipIS= new GZIPInputStream(fis);
+        FileOutputStream fos=new  FileOutputStream(fileDirectory+"\\Decompressedfilecheck");
+        
+        byte[] buffer=new byte[1024];
+        
+        int len;
+        
+        while((len=gzipIS.read(buffer)) !=-1){
+            System.out.println("length of file"+len);
+            fos.write(buffer, 0,len);     
+        }
+        gzipIS.close();
+        fis.close();
+        fos.close();     
+    }
+    
+    public static void main(String[] args) throws IOException{
+        File path=new File("C:\\ASHISH\\PROJECTS\\Compress-Decompress\\Compressedfile.gz");
+        method(path);
+        //System.out.print();
+        
+    }   
+    
+    
+}
